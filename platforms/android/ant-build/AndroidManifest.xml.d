fb874404ed43e0baec52883b7b812d33b6f721d2
YuLun Shih <shih@yulun.me>
2014-07-25 02:01:27 +0800

    remove ant-*

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-H:\Copy\Dropbox\project\mobileapp\tnua-bus\platforms\android\ant-build\AndroidManifest.xml : \
-H:\Copy\Dropbox\project\mobileapp\tnua-bus\platforms\android\AndroidManifest.xml \
--- platforms/android/ant-gen/R.java.d
+++
@@ -1,5 +0,0 @@
-H:\Copy\Dropbox\project\mobileapp\tnua-bus\platforms\android\ant-gen\zack\tnuabus\R.java \
- : H:\Copy\Dropbox\project\mobileapp\tnua-bus\platforms\android\res\drawable\icon.png \
-H:\Copy\Dropbox\project\mobileapp\tnua-bus\platforms\android\res\values\strings.xml \
-H:\Copy\Dropbox\project\mobileapp\tnua-bus\platforms\android\res\xml\config.xml \
-H:\Copy\Dropbox\project\mobileapp\tnua-bus\platforms\android\ant-build\AndroidManifest.xml \
