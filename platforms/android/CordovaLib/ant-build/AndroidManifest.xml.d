12317313e152e53cb254b56aba12caa77e862909
YuLun Shih <shih@yulun.me>
2014-07-25 21:13:34 +0800

    revert

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+H:\Copy\Dropbox\project\mobileapp\tnua-bus\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+H:\Copy\Dropbox\project\mobileapp\tnua-bus\platforms\android\CordovaLib\AndroidManifest.xml \
