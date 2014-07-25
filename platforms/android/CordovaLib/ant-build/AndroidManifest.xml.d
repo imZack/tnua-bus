ea7c3a5f17f62745a552b843d72eece00d6a5a12
YuLun Shih <shih@yulun.me>
2014-07-25 21:43:39 +0800

    Revert "revert"
    This reverts commit 12317313e152e53cb254b56aba12caa77e862909.

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-H:\Copy\Dropbox\project\mobileapp\tnua-bus\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-H:\Copy\Dropbox\project\mobileapp\tnua-bus\platforms\android\CordovaLib\AndroidManifest.xml \
