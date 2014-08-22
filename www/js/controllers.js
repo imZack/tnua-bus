2629b4f4957471da9c9d0ce237b71abd87c8e87f
YuLun Shih <shih@yulun.me>
2014-08-22 22:27:54 +0800

    Update timetable.html
    Using ionic's new feature "Collection Repeat" instead of regular ng-repeat

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -125,6 +125,11 @@ angular.module('tnua-bus.controllers', [])
 	  });
 	}
 
+	$scope.getItemHeight = function(item, index) {
+		//Make evenly indexed items be 10px taller, for the sake of example
+		return 50;
+	};
+
 	// bootstrap
 	$scope.load();
 })
