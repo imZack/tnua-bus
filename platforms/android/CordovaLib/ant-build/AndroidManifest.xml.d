c15cca2a01fe43a740720403b18f20f011a927d1
YuLun Shih <shih@yulun.me>
2014-10-11 12:59:16 +0800

    Remove platforms files

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/zack/Dropbox/project/mobileapp/tnua-bus/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/zack/Dropbox/project/mobileapp/tnua-bus/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /home/zack/Dropbox/project/mobileapp/tnua-bus/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
